a99028cfd8083690093748ddcc753fc84c2fdb30
momoekohan <115914485+momoekohan@users.noreply.github.com>
2022-10-16 14:54:10 +0900

    Initial commit

---
+++ resources/views/top.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<html>
+
+	<head>
+	
+		<meta charset="UTF-8">
+		<title>MOTHER'S WORLD</title>
+		
+	</head>
+
+
+	<body>
+	
+		<h1>ようこそ<br><span id = "maintitle">MOTHER</span>の世界へ！</h1>
+		
+		<div class = "first">
+			<a href = "mother_w.html">？MOTHERってどんなゲーム？</a>
+		</div>
+		
+		<div class = "main">
+			<p class = "question">どの世界に行きますか？</p>
+			
+			<div class = "link">
+				<a href = "mother1.html">まざー１</a>
+				<a href = "mother2.html">まざー２</a>
+				<a href = "mother3.html">まざー３</a>
+			</div>
+		</div>
+		
+		
+	</body>
+	
+</html>
